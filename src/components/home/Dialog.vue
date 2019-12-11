e7e4e20ad0f3779ce260e55d7d7ed5b065a0c7d9
xuzhida <xuzhida@163.com>
2019-12-11 20:17:49 +0800

    完善-搜索城市页面

--- src/components/home/Dialog.vue
+++ src/components/home/Dialog.vue
@@ -17,20 +17,16 @@
       </div>
       <!-- 选择城市 -->
       <div v-if="this.isCity" class="choice-city">
-        <div class="city-search">
-          <input type="text">
-          <button>搜索</button>
-        </div>
         <div class="city-position">
-          <div>定位/最近访问</div>
-          <div>
+          <div style="text-align:left">定位/最近访问:</div>
+          <div class="city-position-items">
             <span>长沙</span>
             <span>怀化</span>
           </div>
         </div>
         <div class="city-hot">
-          <div>热门城市</div>
-          <div>
+          <div style="text-align:left">热门城市:</div>
+          <div class="city-hot-items">
             <span>北京</span>
             <span>上海</span>
             <span>广州</span>
@@ -39,9 +35,16 @@
             <span>重庆</span>
           </div>
         </div>
-        <div>
-          A
-          B
+        <div class="city-search">
+          <input type="text">
+          <button>搜索</button>
+        </div>
+        <div class="city-search-items">
+          <span>A</span>
+          <span>鞍山</span>
+          <span>安阳</span>
+          <span>安庆</span>
+          <span>安康</span>
         </div>
       </div>
     </el-dialog>
@@ -86,5 +89,33 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+}
+.city-position,.city-hot-items{
+  padding: 5px;
+  border-bottom: 1px solid gray;
+}
+.city-position-items,.city-hot-items{
+  flex-flow: wrap;
+  display: flex;
+}
+.city-position-items span,.city-hot-items span {
+  display: inline-block;
+  max-width: 60px;
+  padding: 4px;
+  margin: 3px;
+  border: 1px solid gray;
+}
+.city-search-items{
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+}
+.city-search-items span{
+  display: block;
+  border-bottom: 1px solid gray;
+  text-align: left;
+}
+.city-search{
+  padding: 5px;
 }
 </style>
