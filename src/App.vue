02e96e5642c38e9c3ff0ad6fa2867d71820bcc13
xuzhida <xuzhida@163.com>
2019-12-11 10:23:45 +0800

    创建-查56

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <el-header>
+      <el-row>
+        <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+          <div class="">
+            <span>当前城市</span>
+            <span>长沙市</span>
+            <span>晴 18°C</span>
+          </div>
+        </el-col>
+        <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+          <div class="">
+            <el-button size="small">登录</el-button>
+            <span>关注微信公众号</span>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+@import "/assets/styles/common.css";
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">456
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
