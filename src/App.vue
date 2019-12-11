568061c14fac358511480465b60730a045e39aab
xuzhida <xuzhida@163.com>
2019-12-11 16:25:06 +0800

    完成-主页移动端页面

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,78 @@
 <template>
   <div id="app">
-    <el-header>
-      <el-row>
-        <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
-          <div class="">
-            <span>当前城市</span>
-            <span>长沙市</span>
-            <span>晴 18°C</span>
-          </div>
-        </el-col>
-        <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
-          <div class="">
-            <el-button size="small">登录</el-button>
-            <span>关注微信公众号</span>
-          </div>
-        </el-col>
-      </el-row>
-    </el-header>
+    <el-row class="header-row">
+      <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+        <div class="header-row-left">
+          <span>当前城市:</span>
+          <span>长沙市</span>
+          <span style="margin-left:5px">晴 18°C</span>
+        </div>
+      </el-col>
+      <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+        <div class="header-row-right">
+          <!-- 适应移动端的登录按钮组 -->
+          <el-button size="small" class="setBtn" @click="login()">登录</el-button>
+          <span class="setMiniBtn" @click="login()">登录</span>
+          <span>关注微信公众号</span>
+        </div>
+      </el-col>
+    </el-row>
     <router-view/>
+    <Login :isShow="isShow" @setFalse="setFalse()"></Login>
   </div>
 </template>
 
 <script>
+import Login from './components/home/Login'
 export default {
-  name: "App"
+  name: "App",
+  components:{
+    Login
+  },
+  data(){
+    return{
+      isShow:false
+    }
+  },
+  methods:{
+    login(){
+      this.isShow = true;
+    },
+    setFalse(){
+      this.isShow = false;
+    }
+  }
 };
 </script>
 
 <style>
+/* 响应式样式表按从小到大引入 */
 @import "/assets/styles/common.css";
+@import "/assets/styles/min320px.css";
+@import "/assets/styles/min768px.css";
+@import "/assets/styles/min1000px.css";
+
 #app {
+  height: 100%;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
+.header-row {
+  border-bottom: 1px solid rgba(187, 187, 187, 1);
+}
+.header-row-left,.header-row-right{
+  display: flex;
+}
+.header-row-left{
+  margin-left: 3px;
+  justify-content: flex-start;
+}
+.header-row-right{
+  margin-right: 3px;
+  justify-content: flex-end;
+}
+
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="home">
+    <img :src="this.imageSrc" alt srcset class="home-image">
+    <div class="search-area">
+      <div class="area-tabs">
+        <span>查专线</span>
+        <span class="border">查公司</span>
+        <span>综合信息</span>
+      </div>
+      <div class="area-btn">
+        <input type="text" style="width:80%">
+        <button>搜索</button>
+      </div>
+      <div class="area-history">
+        <span>历史记录:</span>
+        <span>长沙</span>至
+        <span>北京</span>
+      </div>
+    </div>
+    <div class="content">
+      <div class="banner">
+        <img
+          v-for="item in bannerList"
+          :key="item.id"
+          :src="item.imageSrc"
+          alt="广告图片"
+          srcset
+          class="home-image2"
+        >
+      </div>
+      <div class="member-line">
+        <div class="cityArea">
+          <span v-for="item in cityArea" :key="item.id">{{item.name}}</span>
+        </div>
+        <div class="company">
+          <div class="company-item" v-for="item in companyLsit" :key="item.id">
+            <img :src="item.imageUrl" alt="公司照片" srcset class="company-image">
+            <span>
+              公司名称:
+              <span>{{item.name}}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      // logo
+      imageSrc: require("../../assets/images/logo.png"),
+      // 地区数据
+      cityArea: [
+        { name: "华中", id: 1, path: "#1" },
+        { name: "华南", id: 2, path: "#1" },
+        { name: "华东", id: 3, path: "#1" },
+        { name: "华北", id: 4, path: "#1" },
+        { name: "东北", id: 5, path: "#1" },
+        { name: "西南", id: 6, path: "#1" },
+        { name: "西北", id: 7, path: "#1" }
+      ],
+      // 广告数据
+      bannerList: [
+        { name: 1, imageSrc: require("../../assets/images/logo.png") },
+        { name: 2, imageSrc: require("../../assets/images/logo.png") },
+        { name: 3, imageSrc: require("../../assets/images/logo.png") }
+        // {name:4,imageSrc: require("../../assets/images/logo.png")},
+        // {name:5,imageSrc: require("../../assets/images/logo.png")},
+        // {name:6,imageSrc: require("../../assets/images/logo.png")},
+        // {name:7,imageSrc: require("../../assets/images/logo.png")},
+        // {name:8,imageSrc: require("../../assets/images/logo.png")}
+      ],
+      // 公司数据
+      companyLsit: [
+        {
+          name: "长沙五洲",
+          id: 1,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        },
+        {
+          name: "长沙五洲",
+          id: 2,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        },
+        {
+          name: "长沙五洲",
+          id: 3,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        },
+        {
+          name: "长沙五洲",
+          id: 4,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        },
+        {
+          name: "长沙五洲",
+          id: 5,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        },
+        {
+          name: "长沙五洲",
+          id: 6,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        },
+        {
+          name: "长沙五洲",
+          id: 7,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        },
+        {
+          name: "长沙五洲",
+          id: 8,
+          path: "#1",
+          imageUrl: require("../../assets/images/logo.png")
+        }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+.home,
+.content {
+  height: 100%;
+}
+.area-tabs {
+  display: flex;
+  justify-content: space-around;
+  border-bottom: 1px solid gray;
+}
+.area-tabs span {
+  flex: 1;
+  cursor: pointer;
+}
+.border {
+  border-left: 1px solid gray;
+  border-right: 1px solid gray;
+}
+.area-btn,
+.area-history {
+  margin: 5px;
+  padding: 5px;
+}
+.area-btn {
+  display: flex;
+  justify-content: space-between;
+}
+.area-history {
+  display: flex;
+  align-items: flex-start;
+}
+button {
+  border-radius: 5px;
+  background-color: transparent;
+  border-style: none;
+  border: 1px solid gray;
+}
+.member-line {
+  /* background: #ececd5; */
+  border-top: 1px solid gray;
+}
+.cityArea {
+  display: flex;
+}
+.cityArea span {
+  padding: 0 3px;
+}
+.company {
+  display: flex;
+  flex-flow: wrap;
+  justify-content: center;
+}
+.company-item {
+  margin: 5px;
+  background: #e8e8e5
+}
+</style>
